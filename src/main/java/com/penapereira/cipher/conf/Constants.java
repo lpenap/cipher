1edbe55b959daf862ae850ed1af02f2829447679
Luis Augusto Peña Pereira <lpenap@gmail.com>
2017-11-21 12:39:54 -0400

    Initial commit from legacy code

---
+++ src/main/java/com/penapereira/cipher/conf/Constants.java
@@ -0,0 +1,30 @@
+package com.penapereira.cipher.conf;
+
+
+public class Constants {
+	public static final int BLOCK_SIZE_RSA = 117;
+
+	public static final int KB = 1024;
+
+	public static final String DEFAULT_PROPERTIES_FILE = "cipher.properties.xml";
+
+	public static final String RSA = "RSA";
+
+	public static final String AES = "AES";
+
+	public static final String AES_CBC_PKCS5 = "AES/CBC/PKCS5Padding";
+
+	public static final String DEFAULT_PRIVATE_KEY_FILE = "privateKey";
+
+	public static final String DEFAULT_PUBLIC_KEY_FILE = "publicKey";
+
+	public static final String DEFAULT_FILE = "document.bin";
+
+	public static final String PROPERTIES_PUBLIC_KEY = "publicKey";
+
+	public static final String PROPERTIES_PRIVATE_KEY = "privateKey";
+
+	public static final String PROPERTIES_DEFAULT_FILE = "defaultFile";
+
+	public static final int RSA_KEY_SIZE = 1024;
+}
---
+++ src/main/java/com/penapereira/cipher/data/CipherData.java
@@ -0,0 +1,35 @@
+package com.penapereira.cipher.data;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class CipherData {
+	private PrivateKey privateKey = null;
+
+	private PublicKey publicKey = null;
+
+	public CipherData() {
+
+	}
+
+	public CipherData(PrivateKey privateKey, PublicKey publicKey) {
+		this.privateKey = privateKey;
+		this.publicKey = publicKey;
+	}
+
+	public PrivateKey getPrivateKey() {
+		return privateKey;
+	}
+
+	public void setPrivateKey(PrivateKey privateKey) {
+		this.privateKey = privateKey;
+	}
+
+	public PublicKey getPublicKey() {
+		return publicKey;
+	}
+
+	public void setPublicKey(PublicKey publicKey) {
+		this.publicKey = publicKey;
+	}
+}
