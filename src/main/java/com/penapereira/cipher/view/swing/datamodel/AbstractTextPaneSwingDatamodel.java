6c31dddca2c0af7928271488570ba165bb3872b7
Luis Augusto Peña Pereira <lpenap@gmail.com>
2018-06-26 12:34:10 -0400

    Refactor datamodel
    Changes:
    Created a new level of abstraction to isolate datamodel between
    main component (i.e a tabbedPane) and the component hierarchy added
    to the main component.

---
+++ src/main/java/com/penapereira/cipher/view/swing/datamodel/AbstractTextPaneSwingDatamodel.java
@@ -0,0 +1,34 @@
+package com.penapereira.cipher.view.swing.datamodel;
+
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.util.Pair;
+import com.penapereira.cipher.model.document.Document;
+import com.penapereira.cipher.view.swing.listener.CipherDocumentListener;
+
+public abstract class AbstractTextPaneSwingDatamodel extends AbstractSwingDatamodel {
+
+    private final Logger log = LoggerFactory.getLogger(AbstractTextPaneSwingDatamodel.class);
+
+    protected Pair<JComponent, JComponent> buildDocumentContainerHierarchy(Document doc) {
+        JTextPane textPane = new JTextPane();
+        StyledDocument styledDoc = textPane.getStyledDocument();
+        CipherDocumentListener documentListener = new CipherDocumentListener(this, doc.getId());
+        textPane.setFont(getDocumentContainerFont());
+        try {
+            styledDoc.insertString(0, doc.getText(), styledDoc.getStyle("regular"));
+        } catch (BadLocationException e) {
+            log.error("Could not insert text from document '" + doc.getTitle()
+                    + "' into the user interface, skipping...");
+        }
+        styledDoc.addDocumentListener(documentListener);
+        JScrollPane scrollPane = new JScrollPane(textPane);
+        return Pair.of(scrollPane, textPane);
+    }
+
+}
--- src/main/java/com/penapereira/cipher/view/swing/datamodel/TabbedPaneDatamodel.java
+++ src/main/java/com/penapereira/cipher/view/swing/datamodel/TabbedPaneDatamodel.java
@@ -1,21 +1,16 @@
 package com.penapereira.cipher.view.swing.datamodel;
 
 import javax.swing.JComponent;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextPane;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.StyledDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 import com.penapereira.cipher.model.document.Document;
-import com.penapereira.cipher.view.swing.listener.CipherDocumentListener;
 import com.penapereira.cipher.view.swing.search.SearchAdapter;
 
 @Component
-public class TabbedPaneDatamodel extends AbstractSwingDatamodel {
+public class TabbedPaneDatamodel extends AbstractTextPaneSwingDatamodel {
 
     protected final String MODIFIED_PREFIX = "* \u2063";
 
@@ -68,23 +63,6 @@ public class TabbedPaneDatamodel extends AbstractSwingDatamodel {
             }
         }
         return -1;
-    }
-
-    @Override
-    protected Pair<JComponent, JComponent> buildDocumentContainerHierarchy(Document doc) {
-        JTextPane textPane = new JTextPane();
-        StyledDocument styledDoc = textPane.getStyledDocument();
-        CipherDocumentListener documentListener = new CipherDocumentListener(this, doc.getId());
-        textPane.setFont(getDocumentContainerFont());
-        try {
-            styledDoc.insertString(0, doc.getText(), styledDoc.getStyle("regular"));
-        } catch (BadLocationException e) {
-            log.error("Could not insert text from document '" + doc.getTitle()
-                    + "' into the user interface, skipping...");
-        }
-        styledDoc.addDocumentListener(documentListener);
-        JScrollPane scrollPane = new JScrollPane(textPane);
-        return Pair.of(scrollPane, textPane);
     }
 
     @Override
