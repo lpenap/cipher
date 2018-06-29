def96683bf5059b57010603e82682458b60157c2
Luis Augusto Peña Pereira <lpenap@gmail.com>
2018-06-29 12:49:13 -0400

    Add refactor to resue addDocument code
    This code will be reused to provide a rename document feature.

--- src/main/java/com/penapereira/cipher/view/swing/listener/AddDocumentDelegate.java
+++ src/main/java/com/penapereira/cipher/view/swing/listener/AddDocumentDelegate.java
@@ -1,126 +1,35 @@
 package com.penapereira.cipher.view.swing.listener;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
 import com.penapereira.cipher.conf.Messages;
 import com.penapereira.cipher.controller.DocumentController;
-import com.penapereira.cipher.shared.StringUtil;
-import com.penapereira.cipher.view.swing.component.JTextFieldLimit;
 
 public class AddDocumentDelegate extends SingleInputDialogActionListener {
 
     private static final long serialVersionUID = 1L;
     private final Logger log = LoggerFactory.getLogger(AddDocumentDelegate.class);
-    public final static int MAX_INPUT_SIZE = 20;
-    private JTextField documentTitleTextField;
-    private JLabel lblErrorMessage;
 
     public AddDocumentDelegate(DocumentController documentController, Messages messages) {
         super(documentController, messages);
     }
 
     protected void buildDelegate() {
-        setResizable(false);
-        setSize(427, 172);
         setTitle(messages.getAddDocumentMenu());
-        getContentPane().setLayout(new BorderLayout(0, 0));
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setAlignmentX(CENTER_ALIGNMENT);
-        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-
-        addButtons(buttonPanel);
-        addFormPanel();
+        setTextFieldLabel(messages.getDocumentTitle());
+        setErrorText(messages.getDocumentTitleEmpty());
     }
 
-    protected void addFormPanel() {
-        JPanel formPanel = new JPanel();
-        getContentPane().add(formPanel, BorderLayout.CENTER);
-        formPanel.setLayout(new FormLayout(
-                new ColumnSpec[] {FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
-                    FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
-                    ColumnSpec.decode("default:grow"),},
-                new RowSpec[] {FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
-                    FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,}));
-
-        JLabel lblDocumentTitle = new JLabel(messages.getDocumentTitle());
-        formPanel.add(lblDocumentTitle, "4, 4, right, default");
-
-        documentTitleTextField = new JTextField();
-        formPanel.add(documentTitleTextField, "6, 4, left, default");
-        documentTitleTextField.setDocument(new JTextFieldLimit(MAX_INPUT_SIZE));
-        documentTitleTextField.setColumns(MAX_INPUT_SIZE);
-        documentTitleTextField.addFocusListener(new FocusListener() {
-
-            @Override
-            public void focusLost(FocusEvent e) {}
-
-            @Override
-            public void focusGained(FocusEvent e) {
-                lblErrorMessage.setVisible(false);
-            }
-        });
-
-        lblErrorMessage = new JLabel(messages.getDocumentTitleEmpty());
-        lblErrorMessage.setForeground(Color.RED);
-        formPanel.add(lblErrorMessage, "6, 6");
-        lblErrorMessage.setVisible(false);
-    }
-
-    protected void addButtons(JPanel buttonPanel) {
-        JButton btnCreate = new JButton(messages.getCreate());
-        btnCreate.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                addDocument();
-            }
-        });
-        buttonPanel.add(btnCreate);
-
-        JButton btnCancel = new JButton(messages.getCancel());
-        btnCancel.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-            }
-        });
-        buttonPanel.add(btnCancel);
-    }
-
-    protected void addDocument() {
-        String title = new StringUtil().sanitizeString(documentTitleTextField.getText());
-        if (!title.equals("")) {
-            log.info("Adding new empty document with title: " + title);
-            documentController.createAndSaveDocument(title, "");
-            this.setVisible(false);
-        } else {
-            lblErrorMessage.setVisible(true);
-        }
-    }
-
-    /**
-     * This will show the dialog wrapped by this class when the 'Add Document' action is triggered in the parent
-     * component.
-     */
     @Override
     protected void actionPerformedDelegate(ActionEvent e) {
-        lblErrorMessage.setVisible(false);
-        this.setVisible(true);
-        documentTitleTextField.setRequestFocusEnabled(true);
+        getTextField().setRequestFocusEnabled(true);
+    }
+
+    @Override
+    protected void okButtonPressedDelegate(String documentTitle) {
+        log.info("Adding new empty document with title: " + documentTitle);
+        documentController.createAndSaveDocument(documentTitle, "");
     }
 
 }
--- src/main/java/com/penapereira/cipher/view/swing/listener/SingleInputDialogActionListener.java
+++ src/main/java/com/penapereira/cipher/view/swing/listener/SingleInputDialogActionListener.java
@@ -1,12 +1,31 @@
 package com.penapereira.cipher.view.swing.listener;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
 import com.penapereira.cipher.conf.Messages;
 import com.penapereira.cipher.controller.DocumentController;
+import com.penapereira.cipher.shared.StringUtil;
+import com.penapereira.cipher.view.swing.component.JTextFieldLimit;
 
 public abstract class SingleInputDialogActionListener extends AbstractActionListener {
 
     private static final long serialVersionUID = 1L;
+    private final static int MAX_INPUT_SIZE = 20;
+    private JTextField textField;
+    private JLabel lblErrorMessage;
+    protected JLabel lblTextField;
 
     public SingleInputDialogActionListener(DocumentController documentController, Messages messages) {
         super(documentController, messages);
@@ -16,14 +35,116 @@ public abstract class SingleInputDialogActionListener extends AbstractActionList
 
     protected abstract void buildDelegate();
 
+    protected abstract void okButtonPressedDelegate(String text);
+
+    /**
+     * This will show the dialog wrapped by this class when the 'Add Document' action is triggered in the parent
+     * component.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
+        lblErrorMessage.setVisible(false);
+        setVisible(true);
         actionPerformedDelegate(e);
     }
 
     @Override
     protected void build() {
+        setResizable(false);
+        setSize(427, 172);
+        getContentPane().setLayout(new BorderLayout(0, 0));
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setAlignmentX(CENTER_ALIGNMENT);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
+        addButtons(buttonPanel);
+        addFormPanel();
         buildDelegate();
+    }
+
+    protected void addButtons(JPanel buttonPanel) {
+        JButton btnCreate = new JButton(messages.getCreate());
+        btnCreate.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                okButtonPressed();
+            }
+        });
+        buttonPanel.add(btnCreate);
+
+        JButton btnCancel = new JButton(messages.getCancel());
+        btnCancel.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
+        buttonPanel.add(btnCancel);
+    }
+
+    protected void addFormPanel() {
+        JPanel formPanel = new JPanel();
+        getContentPane().add(formPanel, BorderLayout.CENTER);
+        formPanel.setLayout(new FormLayout(
+                new ColumnSpec[] {FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+                    FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
+                    ColumnSpec.decode("default:grow"),},
+                new RowSpec[] {FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+                    FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,}));
+
+        lblTextField = new JLabel();
+        formPanel.add(lblTextField, "4, 4, right, default");
+
+        textField = new JTextField();
+        formPanel.add(textField, "6, 4, left, default");
+        textField.setDocument(new JTextFieldLimit(MAX_INPUT_SIZE));
+        textField.setColumns(MAX_INPUT_SIZE);
+        textField.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusLost(FocusEvent e) {}
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                lblErrorMessage.setVisible(false);
+            }
+        });
+
+        lblErrorMessage = new JLabel();
+        lblErrorMessage.setForeground(Color.RED);
+        formPanel.add(lblErrorMessage, "6, 6");
+        lblErrorMessage.setVisible(false);
+    }
+
+    protected void okButtonPressed() {
+        String text = new StringUtil().sanitizeString(textField.getText());
+        if (!text.equals("")) {
+            okButtonPressedDelegate(text);
+            this.setVisible(false);
+        } else {
+            lblErrorMessage.setVisible(true);
+        }
+    }
+
+    protected void setTextFieldString(String text) {
+        textField.setText(text);
+    }
+
+    protected String getTextFieldString() {
+        return textField.getText();
+    }
+
+    protected void setErrorText(String text) {
+        lblErrorMessage.setText(text);
+    }
+
+    protected JTextField getTextField() {
+        return textField;
+    }
+
+    protected void setTextFieldLabel(String text) {
+        lblTextField.setText(text);
     }
 
 }
