717ff3b418303516e4a662e282669a4bf5b6f9b5
Luis Augusto Pena Pereira <lpena@makingsense.com>
2022-10-05 21:20:02 -0300

    Update tests to junit5

--- src/test/java/com/penapereira/cipher/model/document/DocumentServiceImplTests.java
+++ src/test/java/com/penapereira/cipher/model/document/DocumentServiceImplTests.java
@@ -1,32 +1,35 @@
 package com.penapereira.cipher.model.document;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import com.penapereira.cipher.CipherApplication;
+import com.penapereira.cipher.HeadlessSpringBootContextLoader;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import com.penapereira.cipher.CipherApplication;
-import com.penapereira.cipher.HeadlessSpringBootContextLoader;
-import com.penapereira.cipher.model.document.DocumentRepository;
-import com.penapereira.cipher.model.document.DocumentServiceImpl;
 
-@RunWith(MockitoJUnitRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 @ContextConfiguration(classes = CipherApplication.class, loader = HeadlessSpringBootContextLoader.class)
 public class DocumentServiceImplTests {
 
-    @Mock
+    @MockBean
     DocumentRepository documentRepositoryMock;
 
-    @InjectMocks
-    DocumentServiceImpl documentService;
+    @Autowired
+    private DocumentService documentService;
 
     @Test
     public void testCreate() {
@@ -48,15 +51,16 @@ public class DocumentServiceImplTests {
     public void testFindById() {
         Optional<Document> doc = Optional.of(getTestDocument(Long.MAX_VALUE, "test title", "test text"));
         when(documentRepositoryMock.findById(Long.MAX_VALUE)).thenReturn(doc);
-        assertEquals(doc.get(), documentService.findById(Long.MAX_VALUE));
-        assertEquals(doc.get(), documentService.findById(Long.MAX_VALUE));
+        Optional<Document> foundDoc = documentService.findById(Long.MAX_VALUE);
+        assertNotNull(foundDoc);
+        assertEquals(doc, foundDoc);
     }
 
     protected List<Document> getTestList() {
         List<Document> testList = new ArrayList<>();
         testList.add(getTestDocument(Long.MIN_VALUE, "titleMin", "textMin"));
-        testList.add(getTestDocument(new Long(12), "title02", "text02"));
-        testList.add(getTestDocument(new Long(55), "title03", "text03"));
+        testList.add(getTestDocument(12L, "title02", "text02"));
+        testList.add(getTestDocument(55L, "title03", "text03"));
         testList.add(getTestDocument(Long.MAX_VALUE, "titleMax", "textMax"));
         return testList;
     }
